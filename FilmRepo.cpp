26baee173270bcad7b7eec3e6cd0fc2f4e3a4c92
AndreiMilosteanu <61746614+AndreiMilosteanu@users.noreply.github.com>
2020-04-30 19:13:29 +0300

    Film Repository

---
+++ FilmRepo.cpp
@@ -0,0 +1,114 @@
+#include "FilmRepo.h"
+using namespace std;
+
+bool FilmRepo::findFilm(Film aux) {
+    for(int i=0;i<filme.size();i++)
+        if(filme[i] == aux)
+            return true;
+    return false;
+}
+
+void FilmRepo::addFilm(Film aux) {
+    if(findFilm(aux))
+        cout<<"Filmul exista deja, nu a fost aduagat nimic in lista!"<<endl;
+    else
+        filme.push_back(aux);
+}
+void FilmRepo::delFilm(Film aux) {
+    if(!findFilm(aux))
+        cout<<"Filmul cautat nu exista in lista data!"<<endl;
+    else
+        for(int i=0;i<filme.size();i++)
+            if(filme[i] == aux) {
+                filme.erase(filme.begin() + i);
+                cout << "Filmul a fost sters cu succes!" << endl;
+                break;
+            }
+}
+void FilmRepo::updateTitel(Film aux, string titel_aux) {
+    if(!findFilm(aux))
+        cout<<"Filmul cautat nu exista in lista data!"<<endl;
+    else
+    {
+        for(int i=0;i<filme.size();i++)
+            if(filme[i] == aux) {
+                aux.setTitel(titel_aux);
+                if (findFilm(aux)){
+                    cout << "Filmul nu poate fi actualizat pentru ca mai exista un film identic" << endl;
+                } else{
+                    cout<<"Filmul a fost actualizat cu succes!";
+                    filme[i].setTitel(titel_aux);
+                }
+                break;
+            }
+    }
+}
+void FilmRepo::updateURL(Film aux, string URL_aux) {
+    if(!findFilm(aux))
+    cout<<"Filmul cautat nu exista in lista data!"<<endl;
+    else {
+        for (int i = 0; i < filme.size(); i++)
+            if (filme[i] == aux) {
+                aux.setURL(URL_aux);
+                if (findFilm(aux)) {
+                    cout << "Filmul nu poate fi actualizat pentru ca mai exista un film identic" << endl;
+                } else {
+                    cout << "Filmul a fost actualizat cu succes!";
+                    filme[i].setURL(URL_aux);
+                }
+                break;
+            }
+    }
+}
+void FilmRepo::updateErscheinungsJahr(Film aux, int ErscheingunsJahr_aux) {
+    if(!findFilm(aux))
+        cout<<"Filmul cautat nu exista in lista data!"<<endl;
+    else {
+        for(int i = 0; i < filme.size(); i++)
+            if (filme[i] == aux) {
+                aux.setErscheinungsJahr(ErscheingunsJahr_aux);
+                if (findFilm(aux)) {
+                    cout << "Filmul nu poate fi actualizat pentru ca mai exista un film identic" << endl;
+                } else {
+                    cout << "Filmul a fost actualizat cu succes!";
+                    filme[i].setErscheinungsJahr(ErscheingunsJahr_aux);
+                }
+                break;
+            }
+    }
+}
+void FilmRepo::updateGenre(Film aux, string Genre_aux) {
+    if(!findFilm(aux))
+        cout<<"Filmul nu exista in lista data!"<<endl;
+    else{
+        for(int i=0;i<filme.size();i++)
+            if(filme[i] == aux) {
+                filme[i].setGenre(Genre_aux);
+                cout << "Filmul a fost actualizat cu succes!" << endl;
+                break;
+            }
+    }
+}
+void FilmRepo::updateLikes(Film aux, double likes_aux) {
+    if(!findFilm(aux))
+        cout<<"Filmul nu exista in lista data!"<<endl;
+    else{
+        for(int i=0;i<filme.size();i++)
+            if(filme[i] == aux) {
+                filme[i].setLikes(likes_aux);
+                cout << "Filmul a fost actualizat cu succes!" << endl;
+                break;
+            }
+    }
+}
+void FilmRepo::getAll() {
+    for(int i=0;i<filme.size();i++)
+        filme[i].printFilm();
+}
+vector<Film> FilmRepo::getGenre(string Genre) {
+    vector<Film> aux;
+    for(int i=0;i<filme.size();i++)
+        if(filme[i].getGenre() == Genre)
+            aux.push_back(filme[i]);
+    return aux;
+}
---
+++ FilmRepo.h
@@ -0,0 +1,47 @@
+#ifndef LAB5_FILMREPO_H
+#define LAB5_FILMREPO_H
+
+#include "Film.h"
+#include <vector>
+class FilmRepo{
+private:
+    vector<Film> filme;
+public:
+    /*
+     * se parcurge vecorul de filme si se returneaza true daca sa gaseste o aparitie, daca nu ,false
+     */
+    bool findFilm(Film aux);
+    /*
+     * se cauta filmul in vector
+     * daca exista, se afiseaza eroare pe ecran
+     * daca nu, se adauga in vector
+     */
+    void addFilm(Film aux);
+    /*
+     * se cauta filmul in vector
+     * daca se gaseste, se sterge aparitia
+     * daca nu, se returneaza eroare
+     */
+    void delFilm(Film aux);
+    /*
+     * se cauta filmul care doreste a fi updatat(aux)
+     * daca nu se gaseste, se afiseaza eroare
+     * daca se gaseste, se verfica daca ar exista doua filme identice in cazul updatarii
+     * daca totul este ok, se updateaza si se afiseaza un mesaj
+     * daca nu, se afiseaza eroare
+     */
+    void updateTitel(Film aux, string titel_aux);
+    void updateErscheinungsJahr(Film aux, int ErscheingunsJahr_aux);
+    void updateLikes(Film aux, double likes_aux);
+    void updateURL(Film aux, string URL_aux);
+    void updateGenre(Film aux, string Genre_aux);
+    /*
+     * se printeaza fiecare film din vector
+     */
+    void getAll();
+    /*
+     * se returneaza vectorul de filme care apartin genului dorit
+     */
+    vector<Film> getGenre(string Genre);
+};
+#endif //LAB5_FILMREPO_H
